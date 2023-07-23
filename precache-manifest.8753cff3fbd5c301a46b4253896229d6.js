32be7b1744b3c1ab48d3af1db9fe85db45c615a8
Lazypelt2 <nicoweberhofer@gmail.com>
2023-07-23 22:37:21 +0200

    Updates

---
+++ precache-manifest.8753cff3fbd5c301a46b4253896229d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "970f640aa627c02a496f",
+    "url": "/duoprofiles/static/css/main.1904a7a2.chunk.css"
+  },
+  {
+    "revision": "970f640aa627c02a496f",
+    "url": "/duoprofiles/static/js/main.970f640a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/duoprofiles/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "4514541f2602bdad7966",
+    "url": "/duoprofiles/static/js/2.4514541f.chunk.js"
+  },
+  {
+    "revision": "30f72b9022915b8a83c6",
+    "url": "/duoprofiles/static/js/runtime~main.30f72b90.js"
+  },
+  {
+    "revision": "ac7383ffa6b0910e46dbc71eb10bec2a",
+    "url": "/duoprofiles/index.html"
+  }
+];

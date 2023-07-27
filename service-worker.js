854d11a6299be37f747f585523a7ac3a444956b1
Lazypelt2 <nicoweberhofer@gmail.com>
2023-07-27 20:46:52 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c491598905b76ffa41a032440337e685.js"
+  "/precache-manifest.619012b0cb2416fc8e7a6cc99b0dff5e.js"
 );
 
 workbox.clientsClaim();

c09c7b3fcd4331381896017265f26bab226aa0cb
Lazypelt2 <nicoweberhofer@gmail.com>
2023-07-29 19:49:51 +0200

    Updates

---
+++ precache-manifest.420cb9cf4145353496bdf0c58e55a6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e8846f2553e31255b0e",
+    "url": "/static/css/main.8d32c17b.chunk.css"
+  },
+  {
+    "revision": "0e8846f2553e31255b0e",
+    "url": "/static/js/main.0e8846f2.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "8e9b735b2a54390b0183",
+    "url": "/static/js/2.8e9b735b.chunk.js"
+  },
+  {
+    "revision": "439225c8a17b56244b87",
+    "url": "/static/js/runtime~main.439225c8.js"
+  },
+  {
+    "revision": "c47493559ee7d644e74dcafdb6b5ee4e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37ab86ad90e5a7c19cb085b58cc8c896.js"
+  "/precache-manifest.420cb9cf4145353496bdf0c58e55a6ee.js"
 );
 
 workbox.clientsClaim();
